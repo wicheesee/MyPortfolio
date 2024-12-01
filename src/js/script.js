2ef298c25b6ce9f81c637e1a3ebaf6a54a51c889
wicheesee <144334546+wicheesee@users.noreply.github.com>
2024-12-01 18:57:24 +0700

    pembaharuan

--- src/js/script.js
+++ src/js/script.js
@@ -107,3 +107,59 @@ function toggleModal5() {
         navbar.classList.remove('pointer-events-none'); // Mengaktifkan kembali navbar saat modal ditutup
     }
 }
+
+function toggleModal6() {
+    const modal = document.getElementById('projectModal6');
+    const navbar = document.getElementById('navbar');
+    
+    // Toggle modal visibility
+    modal.classList.toggle('hidden');
+    
+    // Toggle overflow-hidden pada body untuk mencegah scroll saat modal terbuka
+    document.body.classList.toggle('overflow-hidden');
+    
+    // Menambahkan atau menghapus kelas pointer-events-none pada navbar
+    if (!modal.classList.contains('hidden')) {
+        navbar.classList.add('pointer-events-none'); // Menonaktifkan navbar saat modal terbuka
+    } else {
+        navbar.classList.remove('pointer-events-none'); // Mengaktifkan kembali navbar saat modal ditutup
+    }
+}
+
+
+const modalCheckbox = document.getElementById("my_modal_7");
+const navbar = document.getElementById("navbar");
+
+// Fungsi untuk toggle kelas pada navbar
+modalCheckbox.addEventListener("change", () => {
+    if (modalCheckbox.checked) {
+        navbar.classList.add("navbar-disabled");
+    } else {
+        navbar.classList.remove("navbar-disabled");
+    }
+});
+
+const modalCheckbox1 = document.getElementById("my_modal_8");
+const navbar1 = document.getElementById("navbar");
+
+// Fungsi untuk toggle kelas pada navbar
+modalCheckbox.addEventListener("change", () => {
+    if (modalCheckbox1.checked) {
+        navbar1.classList.add("navbar-disabled");
+    } else {
+        navbar1.classList.remove("navbar-disabled");
+    }
+});
+
+
+const modalCheckbox2 = document.getElementById("my_modal_9");
+const navbar2 = document.getElementById("navbar");
+
+// Fungsi untuk toggle kelas pada navbar
+modalCheckbox.addEventListener("change", () => {
+    if (modalCheckbox2.checked) {
+        navbar2.classList.add("navbar-disabled");
+    } else {
+        navbar2.classList.remove("navbar-disabled");
+    }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
       padding: '16px',
     },
     extend: {
+      boxShadow: {
+        neon: "0 0 5px theme('colors.indigo.400'), 0 0 20px theme('colors.indigo.950')"
+      },
       colors: {
         primary: '#818CF8',
         secondary: '#9ca3af',
@@ -23,9 +26,24 @@ module.exports = {
           '0%': { transform: 'rotate(0deg)' },
           '100%': { transform: 'rotate(360deg)' },
         },
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+          },
+        },
       },
       animation: {
         'vinyl-spin': 'vinylSpin 3s linear infinite',
+        blob: "blob 5s infinite",
       },
     },
   },
